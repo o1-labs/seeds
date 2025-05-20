c7ae554701ca27b9ff29a5548cef976304f2f067
SanabriaRusso <sanabriarusso@gmail.com>
2025-05-20 14:59:08 +0200

    OCaml sync check attempt: Rolling back changes to mina-sync-monitor.sh, as they are now going to be tested for both sync tests

--- scripts/mina-sync-monitor.sh
+++ scripts/mina-sync-monitor.sh
@@ -5,17 +5,31 @@ max_attempts=40
 attempt=0
 status="Null"
 sleep_duration=500
-sync_check_command="docker exec openmina curl -s http://localhost:3000/status | jq -r .transition_frontier.sync.status"
+sync_check_command_ocaml="docker exec mina mina client status --json | jq -r .sync_status"
+sync_check_command_rust="docker exec openmina curl -s http://localhost:3000/status | jq -r .transition_frontier.sync.status"
 
 check_sync_status() {
-  status=$(eval "$sync_check_command" || echo "Null")
+  if [ "$1" == "ocaml" ]; then
+    status=$(eval "$sync_check_command_ocaml" || echo "Null")
+  elif [ "$1" == "rust" ]; then
+    status=$(eval "$sync_check_command_rust" || echo "Null")
+  else
+    status="Null"
+  fi
   echo "Current sync status: $status"
 }
 
 # Loop to check sync status
 while [ $attempt -lt $max_attempts ]; do
-  check_sync_status
-  
+  if [ "$1" == "ocaml" ]; then
+    check_sync_status "ocaml"
+  elif [ "$1" == "rust" ]; then
+    check_sync_status "rust"
+  else
+    echo "Invalid container type. Please specify 'ocaml' or 'rust'."
+    exit 1
+  fi
+
   if [ "$status" == "Synced" ]; then
     echo "Mina client is synced."
     exit 0
